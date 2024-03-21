118252eca7d506389ead58ce7fc6ae2d79528f0a
Ian Chan <ianchan1108@hotmail.com>
2024-03-20 17:35:58 -0700

    Hello World!

---
+++ desktop/src/ca/bcit/comp2522/termproject/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2522.termproject;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ca.bcit.comp2522.termproject.COMP2522TermProject;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("JavaTermProject");
+		new Lwjgl3Application(new COMP2522TermProject(), config);
+	}
+}
