0783de48749dd0b95778377ebf3c1e5c1b4a6d99
Ian Chan <ianchan1108@hotmail.com>
2024-03-24 16:22:07 -0700

    Create object image, camera alignment, image projection

--- core/src/ca/bcit/comp2522/termproject/COMP2522TermProject.java
+++ core/src/ca/bcit/comp2522/termproject/COMP2522TermProject.java
@@ -4,13 +4,20 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class COMP2522TermProject extends ApplicationAdapter {
 	private Texture dropImage;
 	private Texture bucketImage;
 	private Sound dropSound;
 	private Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
 
 	@Override
 	public void create() {
@@ -18,10 +25,35 @@ public class COMP2522TermProject extends ApplicationAdapter {
 		dropImage = new Texture(Gdx.files.internal("bucket.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
 
+
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800 , 600);
+
+		batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
+
 	}
 }
