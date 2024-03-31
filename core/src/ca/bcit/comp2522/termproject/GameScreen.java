e5174d5d7acc774d5dac338067957555c8209048
Ian Chan <ianchan1108@hotmail.com>
2024-03-31 16:32:11 -0700

    Add dispose in GameScreen

--- core/src/ca/bcit/comp2522/termproject/GameScreen.java
+++ core/src/ca/bcit/comp2522/termproject/GameScreen.java
@@ -1,13 +1,19 @@
 package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class GameScreen implements Screen {
     final Drop game;
@@ -45,6 +51,20 @@ public class GameScreen implements Screen {
 
         bucket.width = 64; // 64 pixels
         bucket.height = 64; // 64 pixels
+
+        // create object array (raindrop as an example) and spawn the first object
+        raindrops = new Array<Rectangle>();
+        spawnRainDrop();
+    }
+
+    private void spawnRainDrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 600;
+        raindrop.width = 64; // 64 pixels wide
+        raindrop.height = 64; // 64 pixels height
+        raindrops.add(raindrop); // add the raindrops into the array
+        lastDropTime = TimeUtils.nanoTime(); // Calculates the time from when the last object dropped
     }
 
     @Override
@@ -53,12 +73,67 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void render(float v) {
+    public void render(float delta) {
+        // Clear the screen with dark blue colour. Arguments to clear are red, green, blue, and alpha
+        // component in the range [0, 1] of the colour to be used to clear the screen
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
+        // get camera to update its matrices
+        camera.update();
+
+        // tell the SpriteBatch to render in the coordinate system specified by the camera
+        game.batch.setProjectionMatrix(camera.combined);
+
+        // begin a new batch of objects, draw the bucket and all drops
+        game.batch.begin();
+        game.font.draw(game.batch, "Drops Collected: " + dropsGathered, 0, 600);
+        game.batch.draw(bucketImage, bucket.x, bucket.y, bucket.width, bucket.height);
+        for (Rectangle raindrop : raindrops) {
+            game.batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        game.batch.end();
+
+        // process user's input
+        // LEFT KEY PRESSED
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        }
+        // RIGHT KEY PRESSED
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        // Ensure bucket stays within screen bounds
+        if (bucket.x < 0) {
+            bucket.x = 0;
+        }
+        if (bucket.x > 800 - 64) {
+            bucket.x = 800 - 64;
+        }
+
+        // Check if game needs to create new object (Raindrop)
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+            spawnRainDrop();
+        }
+
+        // Move raindrops, remove any that are beneath bottom edge of screen or that hit the bucket.
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while (iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0) {
+                iter.remove();
+            }
+            if (raindrop.overlaps(bucket)) {
+                dropsGathered++;
+                dropSound.play();
+                iter.remove();
+            }
+        }
     }
 
     @Override
-    public void resize(int i, int i1) {
+    public void resize(int width, int height) {
 
     }
 
@@ -79,6 +154,10 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
 
     }
 }
