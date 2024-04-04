85f38a8a980be6bbb1967a39a9364849a139b06c
Edro Gonzales <edrogonzales@gmail.com>
2024-04-04 16:50:10 -0700

    Add javadocs to COMP2522TermProjectTest.java

--- core/src/ca/bcit/comp2522/termproject/COMP2522TermProject.java
+++ core/src/ca/bcit/comp2522/termproject/COMP2522TermProject.java
@@ -1,15 +1,28 @@
+/**
+ * Initializes Game Instance.
+ *
+ * @author Edro Gonzales
+ * @author Ian Chan
+ * @version 2024
+ *
+ */
+
 package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/* Game window
- */
 public class COMP2522TermProject extends Game {
+	/** Sprites to be used in game. */
 	public SpriteBatch batch;
+
+	/** Fonts to be used in game. */
 	public BitmapFont font;
 
+	/**
+	 * Creates a new instance of COMP2522TermProject
+	 */
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
@@ -17,10 +30,16 @@ public class COMP2522TermProject extends Game {
 		this.setScreen(new MainMenuScreen(this));
 	}
 
+	/**
+	 * Uses super class method render from Game.
+	 */
 	public void render() {
 		super.render();
 	}
 
+	/**
+	 * Gets rid of batch and font to clear out memory.
+	 */
 	public void dispose() {
 		batch.dispose();
 		font.dispose();
