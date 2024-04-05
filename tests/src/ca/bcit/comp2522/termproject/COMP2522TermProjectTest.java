b8da346af3775bb4f8e3250087159de8ab0f837b
Edro Gonzales <edrogonzales@gmail.com>
2024-04-05 11:03:27 -0700

    Initialize unit tests

---
+++ tests/src/ca/bcit/comp2522/termproject/COMP2522TermProjectTest.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2522.termproject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class COMP2522TermProjectTest  {
+    @Test
+    public void testInitializeSpriteAndFont() {
+        assertTrue(true, "This test should work");
+    }
+}
