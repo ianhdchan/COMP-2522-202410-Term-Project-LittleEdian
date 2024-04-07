f1f194648cc848176a436a6540d5032e8bfa7c45
Ian Chan <ianchan1108@hotmail.com>
2024-04-06 17:28:42 -0700

    Add information into Laser Class

--- core/src/ca/bcit/comp2522/termproject/Laser.java
+++ core/src/ca/bcit/comp2522/termproject/Laser.java
@@ -1,4 +1,41 @@
 package ca.bcit.comp2522.termproject;
 
-public class Laser {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class Laser extends Enemy {
+    public Laser(final GameScreen gameScreen) {
+        super(gameScreen);
+    }
+
+    @Override
+    void spawnEnemy() {
+        Rectangle laserBeam = new Rectangle();
+        laserBeam.x = MathUtils.random(0, 800 - 64);
+        laserBeam.y = 600;
+        laserBeam.width = 10; // 10 pixels wide
+        laserBeam.height = 64; // 64 pixels height
+        enemy.add(laserBeam); // add the laserbeam into the array
+        lastSpawnTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    void removeEnemy() {
+        iterEnemy = enemy.iterator();
+        while (iterEnemy.hasNext()) {
+            Rectangle laserDrop = iterEnemy.next();
+            laserDrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (laserDrop.y + 64 < 0) {
+                iterEnemy.remove();
+            }
+            if (laserDrop.overlaps(gameScreen.cowboy)) {
+                gameScreen.dropsGathered++;
+                gameScreen.dropSound.play();
+                iterEnemy.remove();
+            }
+        }
+
+    }
 }
