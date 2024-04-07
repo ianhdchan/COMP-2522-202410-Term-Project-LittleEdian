cea3559f4f30124cfb3497690bfb661794df606e
Ian Chan <ianchan1108@hotmail.com>
2024-04-06 20:19:08 -0700

    Create cowboy class

---
+++ core/src/ca/bcit/comp2522/termproject/Cowboy.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.termproject;
+
+public class Cowboy {
+    protected final GameScreen gameScreen;
+    public Cowboy(GameScreen gameScreen) {
+        this.gameScreen = gameScreen;
+    }
+
+    public void cowboyMovement() {
+
+    }
+}
