0c5c4a39e46adcba317d1b5a277e75433261861c
Ian Chan <ianchan1108@hotmail.com>
2024-04-07 01:11:00 -0700

    Add change if-else scenarios

--- core/src/ca/bcit/comp2522/termproject/ChangeDifficulty.java
+++ core/src/ca/bcit/comp2522/termproject/ChangeDifficulty.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 
 public class ChangeDifficulty {
     protected final GameScreen gameScreen;
-    private float timeSeconds = 0f;
+    protected float timeSeconds = 0f;
     private final float period = 1f;
 
     public ChangeDifficulty(final GameScreen gameScreen) {
@@ -18,15 +18,17 @@ public class ChangeDifficulty {
         // Timer for events
         timeSeconds += Gdx.graphics.getDeltaTime();
         if (timeSeconds > period) {
-            System.out.println(timeSeconds);
-            if (timeSeconds > 5) {
+            if (timeSeconds > 70) {
+                gameScreen.oneSecond = 10000000;
+            } else if (timeSeconds > 60) {
+                gameScreen.oneSecond = 100000000;
+            } else if (timeSeconds > 45) {
+                gameScreen.oneSecond = 500000000;
+            } else if (timeSeconds > 30) {
                 gameScreen.bandit2.xMovement = 800;
                 gameScreen.bandit2.speed = -150;
-
-            } else if (timeSeconds > 30) {
-                gameScreen.oneSecond = 100000000;
             } else if (timeSeconds > 10) {
-                gameScreen.oneSecond = 500000000;
+                gameScreen.oneSecond = 800000000;
             }
         }
     }
