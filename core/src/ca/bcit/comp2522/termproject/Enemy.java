d1578c974bde7efbde5e89cd1f72aa6076babbfa
Ian Chan <ianchan1108@hotmail.com>
2024-04-06 17:28:18 -0700

    Add variables and methods into Enemy Abstract Class

--- core/src/ca/bcit/comp2522/termproject/Enemy.java
+++ core/src/ca/bcit/comp2522/termproject/Enemy.java
@@ -1,5 +1,24 @@
 package ca.bcit.comp2522.termproject;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import java.util.Iterator;
 
 abstract public class Enemy {
+    protected final GameScreen gameScreen;
+    protected Array<Rectangle> enemy;
+    protected Iterator<Rectangle> iterEnemy;
+    protected long lastSpawnTime;
+
+
+    public Enemy(final GameScreen gameScreen) {
+        this.gameScreen = gameScreen;
+        this.enemy = new Array<Rectangle>();
+
+    }
+
+    abstract void spawnEnemy();
+    abstract void removeEnemy();
+
+
 
 }
