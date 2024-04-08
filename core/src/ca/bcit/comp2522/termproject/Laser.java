89eb1a50aaba795ef149e935d19f1f39922ae3b8
Ian Chan <ianchan1108@hotmail.com>
2024-04-08 14:16:32 -0700

    Update laser checkstyle errors

--- core/src/ca/bcit/comp2522/termproject/Laser.java
+++ core/src/ca/bcit/comp2522/termproject/Laser.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import static ca.bcit.comp2522.termproject.GameScreen.SCREEN_X;
+import static ca.bcit.comp2522.termproject.GameScreen.SCREEN_Y;
+
 /**
  * Represents the Laser class for the game.
  * Laser objects serve as enemies in the game, which the player must avoid.
@@ -18,41 +21,51 @@ import com.badlogic.gdx.utils.TimeUtils;
  * @version 2024
  */
 public class Laser extends Enemy {
+    /**
+     * Implement the laser image.
+     */
     protected Texture laserImage;
 
     /**
      * Constructs a new Laser object.
      *
      * @param gameScreen The GameScreen instance.
+     * @param texture The Texture images.
      */
-    public Laser(final GameScreen gameScreen, Texture texture) {
+    public Laser(final GameScreen gameScreen, final Texture texture) {
         super(gameScreen);
         this.laserImage = texture;
     }
 
     @Override
     void spawnEnemy() {
+        final int width = 1;
+        final int height = 40;
+        final int size = 15;
         Rectangle laserBeam = new Rectangle();
-        laserBeam.x = MathUtils.random(0, 800 - 15);
-        laserBeam.y = 600;
-        laserBeam.width = 1;
-        laserBeam.height = 40;
+        laserBeam.x = MathUtils.random(0, SCREEN_X - size);
+        laserBeam.y = SCREEN_Y;
+        laserBeam.width = width;
+        laserBeam.height = height;
         enemy.add(laserBeam);
         lastSpawnTime = TimeUtils.nanoTime();
     }
 
     @Override
     void removeEnemy() {
+        final int size = 64;
+        final int dropRate = 200;
+        final float volume = 0.3F;
         iterEnemy = enemy.iterator();
         while (iterEnemy.hasNext()) {
             Rectangle laserDrop = iterEnemy.next();
-            laserDrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if (laserDrop.y + 64 < 0) {
+            laserDrop.y -= dropRate * Gdx.graphics.getDeltaTime();
+            if (laserDrop.y + size < 0) {
                 iterEnemy.remove();
             }
             if (laserDrop.overlaps(gameScreen.player.cowboy)) {
                 gameScreen.healthPoints--;
-                gameScreen.damageNoise.setVolume(gameScreen.damageNoise.play(), 0.3F);
+                gameScreen.damageNoise.setVolume(gameScreen.damageNoise.play(), volume);
                 iterEnemy.remove();
             }
         }
